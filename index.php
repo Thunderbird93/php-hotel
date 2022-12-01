990d24eb66e93d63e451714210c8babe474f5c99
Thunderbird93 <antircristian@gmail.com>
2022-12-01 15:25:48 +0100

    [add] html

--- index.php
+++ index.php
@@ -49,3 +49,16 @@
         echo $hotel['distance_to_center'];
     };
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP HOTEL</title>
+</head>
+<body>
+    
+</body>
+</html>
